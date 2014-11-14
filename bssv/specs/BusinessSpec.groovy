a72a402985cb0bb297a4def184a138bd6eb3fb7d
jdb-work <jdbellamy@greatdanetrailers.com>
2014-11-13 21:27:46 -0500

    simplified project structure

---
+++ bssv/specs/BusinessSpec.groovy
@@ -0,0 +1,44 @@
+package bssv.specs
+import bssv.CustomerOps
+import spock.lang.Specification
+
+import static org.codehaus.groovy.runtime.GroovyCategorySupport.use
+
+class BusinessSpec extends Specification {
+
+    def "fails service lookup when external"() {
+        given:
+        def ni = NetworkInterface.getByName("en0")
+        expect:
+        println ni.displayName
+        println ni.hardwareAddress
+        println ni.index
+        println ni.loopback
+        println ni.MTU
+        println ni.up
+        println ni.toString()
+        true
+    }
+
+    def "ws-security crededentials are injected by withCreds"() {
+        expect:
+        assert true
+    }
+
+    def "address.city is 'Paris' for customer: 250780"() {
+        given:
+        def street
+        when:
+        use(CustomerOps) {
+            street = "".address()
+        }
+        then:
+        street == null
+    }
+
+    def "Id returns 250780 as java.lang.String"() {
+        given: def entityId
+        when: use(CustomerOps) { entityId = "".id() }
+        then: entityId instanceof String
+    }
+}
--- playground.groovy
+++ playground.groovy
@@ -1,5 +1,3 @@
-
-
 //@Grab(group='xpp3', module='xpp3_min', version='1.1.4c')
 //@Grab(group='com.thoughtworks.xstream', module='xstream', version='1.3')
 import com.thoughtworks.xstream.XStream
@@ -19,7 +17,9 @@ def obj
 new File("john.xml").withOutputStream { out ->
     stream.toXML(msg, out)
 }
+/*
 new File("john.xml").withInputStream { ins ->
     obj = stream.fromXML(ins)
 }
-println obj.dump()
+*/
+println obj
